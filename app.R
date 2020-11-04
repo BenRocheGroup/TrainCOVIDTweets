676560d905b7f821f0551c707cc41ee06505d129
Hugo Gruson <hugo.gruson@protonmail.com>
2020-11-04 11:30:36 +0100

    Integrate feedback from tuesday meeting

--- app.R
+++ app.R
@@ -10,40 +10,62 @@ censor_names <- function(text) {
     gsub("@\\w+", "@█████", text)
 }
 
-list_topics <- c("Topic 1" = "topic1",
-                 "Topic 2" = "topic2",
-                 "Topic 3" = "topic3",
-                 "Topic 4" = "topic4",
-                 "Topic 5" = "topic5")
+icon_with_title <- function(..., title) {
+
+    i <- icon(...)
+    i$attribs$title <- title
+
+    return(i)
+}
+
+library(dplyr)
+list_topics <- tribble(
+    ~ id, ~ label, ~ description,
+    "topic1", "Diffusion et propagation de la maladie", "Foyers et zones de circulation, niveaux de risque, chiffres en France et dans le monde : décès, hospitalisations, cas, situation épidémiologique, modélisation",
+    "topic2", "Connaissance de la maladie", "Origine du virus, de la maladie (y compris les hypothèses discutées : Bill Gates, 5G…), mode de transmission, symptômes (dont asymptomatiques), évolution et mutation du virus",
+    "topic3", "Mesures de contrôle, action gouvernementale", "Confinement, quarantaine , couvre-feu, masques, fermetures (école, magasins, bar…), télétravail, distanciation sociale, attestations de déplacement, gel HA, application de suivi (StopCovid, TousAntiCovid), aides aux entreprises",
+    "topic4", "Prise en charge et traitement", "Dépistage (tests, stratégie), médicaments, protocoles thérapeutiques, vaccin",
+    "topic5", "Impacts sociétaux",  "Impacts économiques, inégalités sociales (santé, scolaires…), tension hospitalière et impact sur les soignants, déprogrammations hospitalières, ruptures de stock, achats de panique, racisme, santé mentale (ennui, stress, solitude, dépression, anxiété)"
+)
 
 ui <- fluidPage(
 
     includeCSS("extra.css"),
 
-    titlePanel("Classify COVID-19 tweets"),
+    titlePanel("Classification des tweets sur COVID-19"),
 
     fluidRow(
 
+        column(12,
+               wellPanel(
+                   lapply(scan("text_intro.txt", character(), sep = "\n", quiet = TRUE), p)
+               )
+        ),
         column(6,
-            lapply(seq_along(list_topics), function(i) {
+               h3("Tweet à classer:"),
+               withSpinner(
+                   htmlOutput("twtext",
+                              container = tags$blockquote,
+                              class = "twitter-tweet"),
+               ),
+               actionButton("cancel", "Ce tweet ne parle pas du COVID-19", class = "btn-warning"),
+        ),
+        column(6,
+            lapply(seq_along(list_topics$id), function(i) {
                 sliderTextInput(
-                    list_topics[i], names(list_topics)[i],
-                    choices = c("Negative", "Neutral", "Positive", "N/A"),
+                    list_topics$id[i],
+                    span(tagList(list_topics$label[i], icon_with_title("question-circle", title = list_topics$description[i]))),
+                    choices = c("Négatif", "Neutre", "Positif", "N/A"),
                     selected = "N/A",
                     force_edges = TRUE,
-                    grid = TRUE
+                    grid = TRUE,
+                    width = "100%"
                 )
             }),
-            actionButton("do", "Next tweet",
-                        width = "50%", class = "btn-primary btn-block"),
-            ),
-        column(6,
-            withSpinner(
-                htmlOutput("twtext",
-                           container = tags$blockquote,
-                           class = "twitter-tweet"),
-            ),
-        )
+           actionButton("skip", "Aucun thème ne convient"),
+           actionButton("do", "Valider", class = "btn-success"),
+
+        ),
     )
 
 )
@@ -63,7 +85,7 @@ server <- function(input, output, session) {
     outfile <- paste0(file.path(outdate, user_id), ".csv")
 
     write(
-        paste(c("id", list_topics), collapse = ","),
+        paste(c("id", list_topics$id), collapse = ","),
         file = outfile
     )
 
@@ -82,7 +104,57 @@ server <- function(input, output, session) {
         })
 
         # Save result in an easily readable format
-        topics_feelings <- vapply(list_topics, function(i) input[[i]], character(1))
+        topics_feelings <- vapply(list_topics$id, function(i) input[[i]], character(1))
+
+        write(
+            paste(c(alltweets$id[d], topics_feelings), collapse = ","),
+            file = outfile,
+            append = TRUE
+        )
+
+    })
+
+    observeEvent(input$skip, ignoreNULL = FALSE, {
+
+        # Reset UI state
+        for (i in list_topics) {
+            updateSliderTextInput(session, i, selected = "N/A")
+        }
+
+        # Select and display tweet
+        d <- sample(nrow(alltweets), 1)
+
+        output$twtext <- renderUI({
+            censor_names(alltweets$texte[d])
+        })
+
+        # Save result in an easily readable format
+        topics_feelings <- rep_len("N/A", length(list_topics$id))
+
+        write(
+            paste(c(alltweets$id[d], topics_feelings), collapse = ","),
+            file = outfile,
+            append = TRUE
+        )
+
+    })
+
+    observeEvent(input$cancel, ignoreNULL = FALSE, {
+
+        # Reset UI state
+        for (i in list_topics) {
+            updateSliderTextInput(session, i, selected = "N/A")
+        }
+
+        # Select and display tweet
+        d <- sample(nrow(alltweets), 1)
+
+        output$twtext <- renderUI({
+            censor_names(alltweets$texte[d])
+        })
+
+        # Save result in an easily readable format
+        topics_feelings <- rep_len("OT", length(list_topics$id))
 
         write(
             paste(c(alltweets$id[d], topics_feelings), collapse = ","),
